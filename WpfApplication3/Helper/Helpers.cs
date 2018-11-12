9a27926114a5cc8b8a27b3d92a3e00fe0744c8db
Вячеслав Мастер <smonster777@gmail.com>
2018-11-13 00:06:50 +0600

    Добавьте файлы проекта.

---
+++ WpfApplication3/Helper/Helpers.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication3.Helper
+{
+    public class Helpers
+    {
+        public static bool SendMail(string to, string subject, string body, 
+            out string errMessage)
+        {
+            //1
+            MailMessage msg = new MailMessage();
+            msg.To.Add(new MailAddress(to));
+            msg.From = new MailAddress("info@kkb.kz");
+            msg.Subject = subject;
+            msg.Body = body;
+            msg.Priority = MailPriority.High;
+
+            //2
+            SmtpClient client = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                EnableSsl = true,
+                Port = 587,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(
+                "pampkin220@gmail.com", "oooo1234")
+            };
+
+            //3
+            try
+            {
+                client.Send(msg);
+                errMessage = "Сообщение отправлено";
+                return true;
+            }
+            catch (Exception ex)
+            {
+                errMessage = ex.Message;
+                return false;
+            }
+        }
+    }
+}
---
+++ WpfApplication3/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SendMail_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (Helper.Helpers.SendMail(tbTo.Text, tbSubject.Text, tbBody.Text, out string msg))
+                ErrMsg.Foreground = new SolidColorBrush(Colors.Green);
+            else
+                ErrMsg.Foreground = new SolidColorBrush(Colors.Red);
+
+            ErrMsg.Text = msg;
+        }
+    }
+}
